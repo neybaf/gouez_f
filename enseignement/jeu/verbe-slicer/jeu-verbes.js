14ebf4cec1228328e2df4c549cc6b3fc0fc13cc7
Neibaf <103755582+neybaf@users.noreply.github.com>
2024-09-09 20:36:36 +0800

    Update
    - Update csv jeux-verbes
    - suppression jeu campus
    - update page index jeu

--- enseignement/jeu/verbe-slicer/jeu-verbes.js
+++ enseignement/jeu/verbe-slicer/jeu-verbes.js
@@ -1,14 +1,15 @@
-let canvas, ctx, score, verbesPremierGroupe, motsDivers;
+let canvas, ctx, score, verbesPremierGroupe, motsDivers, currentVerbes;
 let fallingWords = [];
 let gameInterval;
 let isGameRunning = false;
+let gameSpeed = 50; // Vitesse initiale
 
 function startGame() {
     document.getElementById('popup').style.display = 'none';
     score = 0;
     initGame();
     isGameRunning = true;
-    gameInterval = setInterval(updateGame, 50);
+    gameInterval = setInterval(updateGame, gameSpeed);
 }
 
 function initGame() {
@@ -23,7 +24,7 @@ function initGame() {
         .then(data => {
             verbesPremierGroupe = data.verbesPremierGroupe;
             motsDivers = data.motsDivers;
-            words = [...verbesPremierGroupe, ...motsDivers]; // Fusionner les deux listes
+            currentVerbes = [...verbesPremierGroupe.infinitif, ...motsDivers]; // Commencer par l'infinitif
             spawnWord();
         })
         .catch(error => {
@@ -32,7 +33,7 @@ function initGame() {
 }
 
 function spawnWord() {
-    const word = words[Math.floor(Math.random() * words.length)];
+    const word = currentVerbes[Math.floor(Math.random() * currentVerbes.length)];
     fallingWords.push({
         text: word,
         x: Math.random() * canvas.width,
@@ -43,6 +44,9 @@ function spawnWord() {
 
 function updateGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Met à jour la vitesse et le niveau de difficulté en fonction du score
+    adjustDifficulty();
 
     // Dessiner les mots tombants
     fallingWords.forEach((wordObj, index) => {
@@ -67,8 +71,8 @@ function updateGame() {
 function checkWordClick(x, y) {
     fallingWords.forEach((wordObj, index) => {
         if (x > wordObj.x && x < wordObj.x + ctx.measureText(wordObj.text).width && y > wordObj.y - 30 && y < wordObj.y) {
-            // Si c'est un verbe du premier groupe
-            if (verbesPremierGroupe.includes(wordObj.text)) {
+            // Vérifier si c'est un verbe du premier groupe à n'importe quel temps
+            if (Object.values(verbesPremierGroupe).flat().includes(wordObj.text)) {
                 score++;
                 updateScore();
                 fallingWords.splice(index, 1);
@@ -80,6 +84,31 @@ function checkWordClick(x, y) {
             }
         }
     });
+}
+
+function adjustDifficulty() {
+    // Augmenter progressivement la vitesse et changer la liste des verbes
+    if (score >= 25 && score < 50) {
+        currentVerbes = [...verbesPremierGroupe.participe_passe, ...motsDivers];
+        gameSpeed = 45;
+        clearInterval(gameInterval);
+        gameInterval = setInterval(updateGame, gameSpeed);
+    } else if (score >= 50 && score < 75) {
+        currentVerbes = [...verbesPremierGroupe.futur, ...motsDivers];
+        gameSpeed = 40;
+        clearInterval(gameInterval);
+        gameInterval = setInterval(updateGame, gameSpeed);
+    } else if (score >= 75 && score < 100) {
+        currentVerbes = [...verbesPremierGroupe.imparfait, ...motsDivers];
+        gameSpeed = 35;
+        clearInterval(gameInterval);
+        gameInterval = setInterval(updateGame, gameSpeed);
+    } else if (score >= 100) {
+        currentVerbes = [...verbesPremierGroupe.subjonctif, ...motsDivers];
+        gameSpeed = 30;
+        clearInterval(gameInterval);
+        gameInterval = setInterval(updateGame, gameSpeed);
+    }
 }
 
 function updateScore() {
